006af35e91ba408db3cd2162b5f38ed255dfb1b2
Monjurul Hoque <mch.shuvo.official@gmail.com>
2020-03-08 21:28:43 +0600

    Update UVa-10130 SuperSale.cpp

--- UVa-10130 SuperSale.cpp
+++ UVa-10130 SuperSale.cpp
@@ -1,54 +1,65 @@
-#include<bits/stdc++.h>
-     
+#include <bits/stdc++.h>
+
 using namespace std;
-     
+
 #define fasterInOut ios::sync_with_stdio(false); cin.tie(0);
 #define pb push_back
-#define inf INT_MAX
+#define inf LLONG_MAX
 #define neginf LLONG_MIN
 #define wk cout << "working" << endl
 #define all(x) x.begin(),x.end()
 #define mem(arr,value) memset(arr, value, sizeof(arr))
 #define mod 100000007
-#define F first
-#define S second
-         
+
 typedef long long int ll;     
 typedef pair<int , int>  pii;
 typedef pair<int , pii > piii; 
-typedef priority_queue<pii, vector<pii> , greater<pii> > pq; 
+typedef priority_queue <pii, vector<pii> , greater<pii> > pq;
 
+
+int n,k;
+int p[1003],wt[1003];
+ll dp[1003][35];
+
+ll solve(int i, int w){
+	if(dp[i][w] != -1)
+		return dp[i][w];
+		
+    if(i >= n || w < 0)	
+		return 0;
+		
+	ll res1 = 0;
+	
+	if(wt[i] <= w)
+		res1 = p[i] + solve(i+1, w-wt[i]);
+	ll res2 = solve(i+1, w);
+	
+	dp[i][w] = max(res1,res2);
+	
+	return dp[i][w];
+}
 
 int main(){
-	fasterInOut;
-	
-	int t;
-	
-	cin >> t;
-	
-	while(t--){
-		ll n,q,x;		
-		cin >> n >> q;
+    int T,q;
+
+    scanf("%d",&T);
+
+    for(int t=1; t<=T; t++){
+        scanf("%d",&n);
+
+        for(int i=0; i<n; i++){
+            scanf("%d %d",&p[i],&wt[i]);
+        }
+        
+		ll ans = 0;
+        scanf("%d",&q);
+        
+        for(int i=0; i<q; i++){
+            scanf("%d",&k);
+            mem(dp,-1);
+            ans += solve(0,k);
+        }
 		
-		ll a[n],even = 0,odd = 0;
-		for(int i=0; i<n; i++){
-			cin >> a[i];
-			
-			if(a[i]%2)
-				odd++;
-			else 
-				even++;
-		}
-		
-		for(int i=0; i<q; i++){
-			cin >> x;
-			
-			if(x%2)
-				cout << odd << " " << even << "\n";
-			else
-				cout << even << " " << odd << "\n";
-		}
-		
-	}
-	return 0;
+        printf("%lld\n",ans);
+    }
 }
